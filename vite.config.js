faa582eaa10f80e2d4da57f24006d86d2a5e88fd
Fede97Ubi <fede.rome97@hotmail.it>
2024-09-27 17:07:31 +0200

    aggiunta codice in index seguendo la guida

--- vite.config.js
+++ vite.config.js
@@ -16,15 +16,26 @@ export default defineConfig({
         short_name: "Vite PWA Project",
         icons: [
           {
-            src: "src/icon/icon-512x512.png",
-            size: "512x512",
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "src/icon/icon-192x192.png",
-            size: "192x192",
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
             type: "image/png",
+            purpose: "maskable",
           },
         ],
       },
