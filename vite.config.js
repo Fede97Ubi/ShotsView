e4071e54e36cfdb9d1dfa10b75d0bbd349ed5d49
Fede97Ubi <fede.rome97@hotmail.it>
2024-09-27 16:33:54 +0200

    test con guida https://www.saurabhmisra.dev/setup-react-pwa-using-vite/

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,29 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        description: "testmanifest",
+        theme_color: "#aaafff",
+        name: "Vite PWA Project",
+        short_name: "Vite PWA Project",
+        icons: [
+          {
+            src: "src/icon/icon-512x512.png",
+            size: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "src/icon/icon-192x192.png",
+            size: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
     // {
     //   manifest: {
     //     description: "testmanifest",
